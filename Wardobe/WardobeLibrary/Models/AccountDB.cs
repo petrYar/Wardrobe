cdfc50f334e8139c416f5ea4131f99bdea4eb7d7
petrYar <yyy1505yyy1505@gmail.com>
2020-05-05 21:04:05 +0300

    (Untested)Adding new classes for EF and for using inside program

---
+++ Wardobe/WardobeLibrary/Models/AccountDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WardobeLibrary.Models
+{
+    [Table("tblAccountDB")]
+    public class AccountDB
+    {
+        [Key]
+        private int id;
+
+        public string IdGuid { get; set; }
+
+        public string UserName { get; private set; }
+
+        public string Password { get; set; }
+
+        private decimal _money;
+
+        public decimal Money { get => _money; private set => this._money = value; }
+
+        public string Token { get; set; }
+
+        [ForeignKey("ClothesOf")]
+        public int IdClothes { get; set; }
+
+
+        public virtual ICollection<ClothesDB> ClothesOf { get; set; }
+    }
+}
