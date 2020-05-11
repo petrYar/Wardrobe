29123be321d1c13ad51c26c06e7a0d33089d881b
Nazar Holub <webblgch@gmail.com>
2020-05-11 20:18:10 +0300

    Fixed bugssss

--- Wardrobe/WardobeClient/LoginWindow.xaml.cs
+++ Wardrobe/WardobeClient/LoginWindow.xaml.cs
@@ -27,18 +27,25 @@ namespace WardobeClient
 
         private async void Button_Click(object sender, RoutedEventArgs e)
         {
-            WardobeClient.Proxy.AccountServiceClient client = new WardobeClient.Proxy.AccountServiceClient();
-            string returnData =await client.LoginAsync(txtLogin.Text, PasswordBox.Password);
-            if (returnData != null)
+            try
             {
-                MainWindow mainWindow = new MainWindow();
-                this.Hide();
-                mainWindow.ShowDialog();
-                this.Close();
+                WardobeClient.Proxy.AccountServiceClient client = new WardobeClient.Proxy.AccountServiceClient();
+                string returnData = await client.LoginAsync(txtLogin.Text, PasswordBox.Password);
+                if (returnData != null)
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    this.Hide();
+                    mainWindow.ShowDialog();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Login or password is incorrect!");
+                }
             }
-            else
+            catch(Exception ex)
             {
-                MessageBox.Show("Login or password is incorrect!");
+                MessageBox.Show(ex.Message);
             }
         }
 
